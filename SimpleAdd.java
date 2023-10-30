cfbeb88337f11d24575b3b35038c0bba320d9695
ram-909 <129027209+ram-909@users.noreply.github.com>
2023-10-30 03:53:31 +0000

    first commit

---
+++ SimpleAdd.java
@@ -0,0 +1,8 @@
+class SimpleAdd {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        System.out.println("The sum of a and b is = " + Integer.sum(a, b));
+    }
+}
